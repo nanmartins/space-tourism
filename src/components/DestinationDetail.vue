ba1034470a41f84a4afacaf230c915e5bb3d11af
nanmartins <renanmartins989@gmail.com>
2023-09-14 19:06:18 +0100

    start on destinations views

---
+++ src/components/DestinationDetail.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="destination-detail">
+    <h2>{{ destination.name }}</h2>
+    <img :src="destination.images.png" alt="Destination Image">
+    <p>{{ destination.description }}</p>
+    <p>Distance: {{ destination.distance }}</p>
+    <p>Travel Time: {{ destination.travel }}</p>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+import data from '../data.json '
+
+const destination = ref({});
+
+onMounted(() => {
+  const destinationName = $route.params.name;
+  // Faça uma solicitação para o arquivo data.json aqui e carregue os dados do destino
+  // Substitua este trecho pelo código real de solicitação de dados
+  const data = {
+    destinations: [
+      // ... (seu array de destinos)
+    ]
+  };
+
+  const selectedDestination = data.destinations.find(dest => dest.name === destinationName);
+
+  if (selectedDestination) {
+    destination.value = selectedDestination;
+  }
+});
+
+</script>
+
+<style>
+
+</style>
--- src/views/Destination.vue
+++ src/views/Destination.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="destination-view">
+    <v-row class="align-center justify-center ma-0">
 
+      <v-col cols="5">
+        <h2><span>01</span> Pick your destination</h2>
+
+      </v-col>
+
+      <v-col cols="5">
+        <div>
+          <router-link to="/destination">Moon</router-link>
+          <router-link to="/destination">Mars</router-link>
+          <router-link to="/destination">Europa</router-link>
+          <router-link to="/destination">Titan</router-link>
+        </div>
+      </v-col>
+    </v-row>
   </div>
 </template>
 

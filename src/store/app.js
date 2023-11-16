eedcd668e0a2fc3dbf7e8658173c06c203e77f08
Renan Martins <renanmartins989@gmail.com>
2023-11-16 03:24:33 +0000

    ...

--- src/store/app.js
+++ src/store/app.js
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia'
 
 export const useAppStore = defineStore('app', {
   state: () => ({
-    drawer: true,
+    drawer: false,
     isScreenSmall: window.innerWidth <= 650
   }),
   actions: {
